566a485e6775dd59f6cf921115c2555232b67dfd
proczero <NavitGizmo@Gmail.com>
2014-11-20 22:54:51 -0500

    Initial Commit
    Initial commit of side projects.

---
+++ Gizmo/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gizmo
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            /* The program checks to see if the directory structure it expects exists, 
+             * if it does not then it sets the flag back to true on FirstRun settings.
+             * 
+             * It then prompts the user if it is alright to make the directory structure
+             * if yes it creates the structures it needs.
+             */
+            GizSetup setup = new GizSetup();
+            if (setup.Validate_MainDir() == false)
+            {
+                Properties.Settings.Default.FirstRun = true;
+                Properties.Settings.Default.Save();
+            }
+            bool first_Run = Properties.Settings.Default.FirstRun;
+            if (first_Run == true)
+            {
+                if (MessageBox.Show(@"This is a first run, or the directory structure was removed. May we create a directory structure at C:\Gizmo\?", "Setup",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    setup.Create_Directory();
+                    Properties.Settings.Default.FirstRun = false;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+    }
+}
---
+++ Gizmo/GizSetup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gizmo
+{
+    class GizSetup
+    {
+        System.Collections.Specialized.StringCollection dirs = Properties.Settings.Default.Dirs;
+
+        public void Create_Directory()
+        {
+            foreach (string dir in dirs)
+            {
+                bool valid_Dir = Directory.Exists(dir);
+                if (valid_Dir == false)
+                {
+                    Directory.CreateDirectory(dir);
+                }
+            }
+        }
+
+        public Boolean Validate_MainDir()
+        {
+            bool validation = Directory.Exists(dirs[0]);
+            return validation;
+        }
+    }
+}
